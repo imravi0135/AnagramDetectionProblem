f98ae39e4b450c6bea3e210a6b27d7ec9de91f70
ravikishorereddy <ravikishorereddy309@gmail.com>
2022-06-10 10:47:46 +0530

    Anagram Detecton

---
+++ AnagramDetection/Anagram.cs
@@ -0,0 +1,37 @@
+﻿//a. Desc ­> One string is an anagram of another if the second is simply a
+//rearrangement of the first. For example, 'heart' and 'earth' are anagrams...
+//b. I/P ­> Take 2 Strings as Input such abcd and dcba and Check for Anagrams
+//c. O/P ­> The Two Strings are Anagram or not....
+using System;
+using System.Collections;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnagramDetection
+{
+     class Anagram
+    {
+        public bool Display(string val1,string val2)
+        {
+            if (val1.Length != val2.Length) 
+            { 
+                return false;
+            }
+            char[] a = val1.ToLower().ToCharArray();
+            char[] b = val2.ToLower().ToCharArray();
+            Array.Sort(a);
+            Array.Sort(b);
+            for(int i = 0; i < a.Length; i++)
+            {
+                if(a[i].ToString()!= b[i].ToString())
+                {
+                    return false;
+                }
+                return true;
+            }
+            return true;
+        }
+    }
+}
---
+++ AnagramDetection/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnagramDetection
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Anagram anagram = new Anagram();
+            Console.WriteLine("Enter the Value1:");
+            string val1 = Console.ReadLine();
+            Console.WriteLine("Enter the Value2:");
+            string val2 = Console.ReadLine();
+            if(anagram.Display(val1,val2))
+            {
+                Console.WriteLine("It is Anagram");
+            }
+            else
+            {
+                Console.WriteLine("Its not a Anagram");
+            }
+            Console.ReadKey();
+        }
+    }
+}
